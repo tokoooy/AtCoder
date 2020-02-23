8006b68fcb65fba18a91bf6d7560cb1e476e8efb
tokoooy <sakana3.app@gmail.com>
2020-02-24 01:38:08 +0900

    main template

--- Main.cpp
+++ Main.cpp
@@ -7,13 +7,18 @@ typedef vector<vi> vvi;
 typedef vector<ll> vll;
 typedef vector<vll> vvll;
 typedef vector<string> vs;
+typedef pair<int, int> P;
 
 #define rep(i, n) for(int i = 0; i < (int)(n); i++)
 #define all(x) (x).begin(),(x).end()
 #define rall(x) (x).rbegin(),(x).rend()
 
+void YN(bool flg){if(flg) cout << "YES" << endl; else cout << "NO" << endl;}
+void Yn(bool flg){if(flg) cout << "Yes" << endl; else cout << "No" << endl;}
+void yn(bool flg){if(flg) cout << "yes" << endl; else cout << "no" << endl;}
+
 int main() {
     
-    
+
     return 0;
 }
