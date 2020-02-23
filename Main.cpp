3c4b20751e0b4df251cbda9753736a2523e37b79
tokoooy <sakana3.app@gmail.com>
2020-02-23 20:00:47 +0900

    Main

--- Main.cpp
+++ Main.cpp
@@ -4,30 +4,16 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> P;
 
-using vi = vector<int>;
-using vvi = vector<vector<int>>;
-using vvvi = vector<vector<vector<int>>>;
-using vll = vector<ll>;
-using vvll = vector<vector<ll>>;
-using vvvll = vector<vector<vector<ll>>>;
-using vd = vector<double>;
-using vvd = vector<vector<double>>;
-using vvvd = vector<vector<vector<double>>>;
-using vb = vector<bool>;
-using vvb = vector<vector<bool>>;
-using vs = vector<string>;
-using pint = pair<int, int>;
-using Graph = vvi;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
 
 #define rep(i, n) for(int i = 0; i < (int)(n); i++)
 #define all(x) (x).begin(),(x).end()
-
-bool cmp(pint a, pint b) {return a.second < b.second;}
-void YN(bool flg) {cout<<(flg?"YES":"NO")<<'\n';}
-void Yn(bool flg) {cout<<(flg?"Yes":"No")<<'\n';}
-void yn(bool flg) {cout<<(flg?"yes":"no")<<'\n';}
-template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
-template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+#define rall(x) (x).rbegin(),(x).rend()
 
 int main() {
     
